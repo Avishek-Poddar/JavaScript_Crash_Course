6e13554fb1ed6a7fe22fb360bf89e7cf0827f3e2
Avishek <avik.leonz@gmail.com>
2022-05-16 18:05:57 +0930

    commit

--- index.js
+++ index.js
@@ -3,3 +3,20 @@
 var age= prompt('What is your age?');
 document.getElementById('av').innerHTML='Your age is '+ age;
 
+// Numbers in JavaScript
+var num1=10;
+
+// Increment num1 by 1 
+num1++;
+console.log(num1);
+
+// Decrement num1 by 1 
+num1--;
+console.log(num1);
+
+//Divide, Multiply *, remainder %
+console.log(num1 %6);
+
+//Increment/Decrement by any number you want
+num1 +=120; //put the number here 
+console.log(num1);
