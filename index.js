fa756c5b196ae7d2f5b6eb24f7c1bd77d7215979
Avishek <avik.leonz@gmail.com>
2022-05-16 18:45:29 +0930

    commit

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
 
 
-var age= prompt('What is your age?');
-document.getElementById('av').innerHTML='Your age is '+ age;
+// var age= prompt('What is your age?');
+// document.getElementById('av').innerHTML='Your age is '+ age;
 
-// Numbers in JavaScript
-var num1=10;
+// // Numbers in JavaScript
+// var num1=10;
 
-// Increment num1 by 1 
-num1++;
-console.log(num1);
+// // Increment num1 by 1 
+// num1++;
+// console.log(num1);
 
-// Decrement num1 by 1 
-num1--;
-console.log(num1);
+// // Decrement num1 by 1 
+// num1--;
+// console.log(num1);
 
-//Divide, Multiply *, remainder %
-console.log(num1 %6);
+// //Divide, Multiply *, remainder %
+// console.log(num1 %6);
 
-//Increment/Decrement by any number you want
-num1 +=120; //put the number here 
-console.log(num1);
+// //Increment/Decrement by any number you want
+// num1 +=120; //put the number here 
+// console.log(num1);
 
 
-// function
-// 1 creating a function
-function fun(){
-  alert('This is a function')
-}
+// // function
+// // 1 creating a function
+// function fun(){
+//   alert('This is a function')
+// }
 
-//call a functinon 
-fun();
+// //call a functinon 
+// fun();
 
-//this is a function
+ function greeting(yourName){
+ var result = 'Hello'+ ' ' + yourName;
+ console.log(result);
+ }
+ var name = prompt ('What is your name?');
+ greeting(name);
+
+// How do argumets work in functions?
+ 
+// function sunNumbers(num1, num2){
+//   var result= num1+ num2;
+//   console.log(result);
+// }
+// sunNumbers(10, 10)
